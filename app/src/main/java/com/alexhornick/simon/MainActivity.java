07943870b831c8a95a96bfc05b8a8d334811e002
Alex Hornick <tennalexd@yahoo.com>
2017-02-19 20:39:32 -0600

    PlayActivity setup, working on design for Play Activity 4 color buttons

--- app/src/main/java/com/alexhornick/simon/MainActivity.java
+++ app/src/main/java/com/alexhornick/simon/MainActivity.java
@@ -1,13 +1,26 @@
 package com.alexhornick.simon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.play_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //start activity based on button clicked
+        if(v.getId()==R.id.play_button){
+            Intent intent=new Intent(this,PlayActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
